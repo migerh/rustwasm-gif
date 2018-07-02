958d6b79f4a2c2c341e2db8c257abec173b4b146
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2018-07-02 21:14:13 +0200

    Improve build script
    The previous version would also delete all *.worker.js and *.wasm files
    in all subdirectories. find can be restricted to the root folder and can
    thus be used to only delete the built files and not the source files.

--- build-pages.sh
+++ build-pages.sh
@@ -3,13 +3,13 @@
 # Clean up previous builds. webpack will generate a main.js, a <hash>.wasm
 # module and one <hash>.worker.js.
 git rm main.js
-git rm *.wasm
-git rm *.worker.js
+find . -maxdepth 1 -name '*.wasm' -exec git rm {} \;
+find . -maxdepth 1 -name '*.worker.js' -exec git rm {} \;
 
 # Rebuild the application
 ./node_modules/.bin/webpack-cli --output-path .
 
 # Add the freshly built js/wasm modules
 git add main.js
-git add *.wasm
-git add *.worker.js
+git add ./*.wasm
+git add ./*.worker.js
